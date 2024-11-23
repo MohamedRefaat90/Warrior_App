a7d5413129a1be22527db9bf7d642e51b57648ad
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-11-23 17:18:53 +0200

    - init login screen

---
+++ lib/features/Auth/presentation/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text('Warrior'),
+                Text('Login'),
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}

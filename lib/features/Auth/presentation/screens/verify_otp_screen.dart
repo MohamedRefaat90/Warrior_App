d7a2f645b7adf57b58f3b81a11824e7f6c2ac9e5
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-11-24 17:35:44 +0200

    Add OTP verification screen with custom input field and timer button

--- lib/features/Auth/presentation/screens/verify_otp_screen.dart
+++ lib/features/Auth/presentation/screens/verify_otp_screen.dart
@@ -1,9 +1,8 @@
 import 'package:Warrior/core/widgets/custom_btn.dart';
+import 'package:Warrior/features/Auth/presentation/widgets/otp_with_shadows.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:otp_timer_button/otp_timer_button.dart';
-import 'package:pinput/pinput.dart';
 
 class VerifyOtpScreen extends StatelessWidget {
   const VerifyOtpScreen({super.key});
@@ -15,37 +14,32 @@ class VerifyOtpScreen extends StatelessWidget {
           title: const Text('Verify OTP'),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            const Text("Enter the OTP sent to your email"),
-            20.verticalSpace,
-            Center(
-              child: Pinput(
-                onCompleted: (pin) => print(pin),
-                showCursor: true,
-                closeKeyboardWhenCompleted: true,
-                length: 6,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              const Text("Enter the OTP sent to your email"),
+              20.verticalSpace,
+              const Center(child: RoundedWithShadow()),
+              20.verticalSpace,
+              OtpTimerButton(
+                // controller: controller,
+                onPressed: () {},
+                buttonType: ButtonType.text_button,
+                textColor: Colors.blue,
+                backgroundColor: Colors.blue,
+                text: const Text('Resend OTP'),
+                duration: 60,
               ),
-            ),
-            20.verticalSpace,
-            OtpTimerButton(
-              // controller: controller,
-              onPressed: () {},
-              buttonType: ButtonType.text_button,
-              textColor: Colors.blue,
-              backgroundColor: Colors.blue,
-              text: const Text('Resend OTP'),
-              duration: 60,
-            ),
-            15.verticalSpace,
-            CustomBTN(
-                widget: const Text('Submit'),
-                padding: 15,
-                width: 150,
-                color: const Color(0xffb4182d),
-                press: () {})
-          ],
+              15.verticalSpace,
+              CustomBTN(
+                  widget: const Text('Submit'),
+                  padding: 15,
+                  width: 150,
+                  color: const Color(0xffb4182d),
+                  press: () {})
+            ],
+          ),
         ));
   }
 }
